ff3d93d2751721e32e33dce350cfbfb1919d410a
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-04-08 23:36:24 +0530

    Php Forms!

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,9 @@
 <?php
 
+$app = [];
+
 // Config
-$config = require 'config.php';
+$app['config'] = require 'config.php';
 
 // Router
 require 'core/Router.php';
@@ -14,5 +16,9 @@ require 'core/Request.php';
 require 'core/database/Connection.php';
 require 'core/database/QueryBuilder.php';
 
-return new QueryBuilder(Connection::make($config['database']));
+$app['database'] = new QueryBuilder(
+  Connection::make($app['config']['database'])
+);
+
+return $app['database'];
 
--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -3,7 +3,7 @@
 class QueryBuilder {
   protected $pdo;
 
-  public function __construct(PDO $pdo) {
+  public function __construct($pdo) {
     $this -> pdo = $pdo;
   }
 
@@ -12,5 +12,21 @@ class QueryBuilder {
     $statement -> execute();
 
     return $statement -> fetchAll(PDO::FETCH_CLASS, $intoClass);
+  }
+
+  public function insert($table, $parameters) {
+    $sql = sprintf(
+      'INSERT INTO %s (%s) VALUES (%s)',
+      $table,
+      implode(', ', array_keys($parameters)),
+      ':' . implode(', :', array_keys($parameters))
+    );
+
+    try {
+      $statement = $this -> pdo -> prepare($sql);
+      $statement -> execute($parameters);
+    } catch (Exception $e) {
+      die('Whoops something went wrong');
+    }
   }
 }
